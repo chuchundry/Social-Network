e656a33ca3b6bd1d72437b683053fcf6b970c091
chuchundry <bektemirovbektur923@gmail.com>
2023-06-26 07:27:04 +0600

    fisrt

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import './Body.css';
+
+const App = () => {
+    return ( <div className='module'>
+     <Header/>
+     <Body/>
+    </div>
+    );
+}
+
+export default App;
